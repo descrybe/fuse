f3eec0959ff386aa5ab4155a655cf9638b01142e
descrybe <descrybe91@gmail.com>
2021-04-01 21:18:01 +0300

    fix for deploy

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <Router>
+  <Router basename="/fuse">
     <App />
   </Router>,
   document.getElementById('root')
